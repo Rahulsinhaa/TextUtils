1a1c8d50265bcb526d3c153f5098119a88f60b0c
2146trisha <108390241+2146trisha@users.noreply.github.com>
2022-07-06 00:43:58 +0530

    Updates

---
+++ precache-manifest.a7f5ac96ba976878f3ec08d6b1749f13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4802f9b3a20b2fea4d05",
+    "url": "/TextUtil/static/css/main.236f641a.chunk.css"
+  },
+  {
+    "revision": "4802f9b3a20b2fea4d05",
+    "url": "/TextUtil/static/js/main.4802f9b3.chunk.js"
+  },
+  {
+    "revision": "8f2f4211b0891106f089",
+    "url": "/TextUtil/static/js/1.8f2f4211.chunk.js"
+  },
+  {
+    "revision": "404adb152457309ce377",
+    "url": "/TextUtil/static/js/2.404adb15.chunk.js"
+  },
+  {
+    "revision": "007f6372142bd9e1cb23",
+    "url": "/TextUtil/static/js/runtime~main.007f6372.js"
+  },
+  {
+    "revision": "1e1cc9b3948a822c9ccd3262401828c3",
+    "url": "/TextUtil/index.html"
+  }
+];
